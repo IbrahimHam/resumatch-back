47b26596794530ce03dbf0324f6e852a65e32cba
Lyutfi <lutfickl@hotmail.com>
2024-08-12 20:02:40 +0200

    added user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,25 @@
 const express = require('express');
 
-const router = express.Router();
+const userRoutes = express.Router();
 
-// Define your routes here
+const userController = require('../controllers/userController');
 
-module.exports = router;
+userRoutes.post('/login', userController.login);
+
+userRoutes.post('/register', userController.register);
+
+userRoutes.post('/upload-resume', userController.uploadResume);
+
+userRoutes.post('/create-resume-data', userController.createResumeData);
+
+userRoutes.put('/update-resume-data', userController.updateResumeData);
+
+userRoutes.get('/templates', userController.getTemplates);
+
+userRoutes.get('/template/:id', userController.getTemplate);
+
+userRoutes.get('/jobs', userController.getJobs);
+
+userRoutes.get('/applied-jobs', userController.getAppliedJobs);
+
+module.exports = userRoutes;
