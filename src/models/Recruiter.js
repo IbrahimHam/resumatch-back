3cba9a091c31cf5a1e3be4ad9c1e0dcfa13bcad6
IbrahimHam <42837548+IbrahimHam@users.noreply.github.com>
2024-08-12 19:43:46 +0200

    Added Resume Model to /models

--- src/models/Recruiter.js
+++ src/models/Recruiter.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const recruiterSchema = new Schema({
+const recruiterSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
---
+++ src/models/Resume.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const resumeSchema = new mongoose.Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    summary: {
+        type: String
+    },
+    skills: [{ type: String }],
+    experience: [{
+        jobTitle: { type: String },
+        company: { type: String },
+        startDate: { type: Date },
+        endDate: { type: Date },
+        description: { type: String }
+    }],
+    education: [{
+        institution: { type: String },
+        degree: { type: String },
+        startDate: { type: Date },
+        endDate: { type: Date }
+    }],
+    languages: [{ type: String }],
+    references: [{ type: String }],
+    certificates: [{
+        institution: { type: String },
+        title: { type: String },
+        date: { type: Date }
+    }]
+});
+
+const Resume = mongoose.model('Resume', resumeSchema);
+
+module.exports = Resume;
