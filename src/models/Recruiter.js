71d8c57f7ab7b8cb9406912c3c14a4ab1d04f90e
IbrahimHam <42837548+IbrahimHam@users.noreply.github.com>
2024-08-12 19:21:53 +0200

    Added Recruiter model to  /models

---
+++ src/models/Recruiter.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const recruiterSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    fullName: {
+        type: String,
+        required: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    postedJobs: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Job'
+    }],
+    companyId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Company'
+    }
+}, {
+    timestamps: true
+});
+
+const Recruiter = mongoose.model('Recruiter', recruiterSchema);
+module.exports = Recruiter;
